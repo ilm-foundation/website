109476a656ded63f597aae474937be6d6a0fc26e
Zafarullah Mahmood <zmahmood@dialpad.com>
2021-08-08 17:46:30 -0400

    Map added

---
+++ js/maps/students.js
@@ -0,0 +1,80 @@
+function get_students() {
+    students = {
+        'Andaman & Nicobar': [],
+        'Andhra Pradesh': [],
+        'Arunachal Pradesh': [],
+        'Assam': [],
+        'Bihar': [
+            {"Name": "Hamid, Munteha & Pakeeza", "City": "Darbhanga", "Class": "N/A"}
+        ],
+        'Chattisgarh': [],
+        'Delhi NCR': [
+            {"Name": "Hammd", "City": "Old Delhi", "Class": "N/A"},
+            {"Name": "Abdul Dayyan", "City": "New Delhi", "Class": "N/A"},
+        ],
+        'Goa': [],
+        'Gujarat': [],
+        'Haryana': [],
+        'Himachal Pradesh': [],
+        'Jammu & Kashmir': [],
+        'Jharkhand': [],
+        'Karnataka': [
+            {"Name": "Ayesha & Thamshera", "City": "Mangalore", "Class": "N/A"},
+            {"Name": "Tasmiya Kousar", "City": "Bengaluru", "Class": "N/A"},
+            {"Name": "Shazan", "City": "Bengaluru", "Class": "N/A"},
+            {"Name": "Musfira", "City": "Bengaluru", "Class": "N/A"},
+            {"Name": "Mujahid Zain", "City": "Bengaluru", "Class": "N/A"},
+            {"Name": "Fathima Misbah", "City": "Bengaluru", "Class": "N/A"},
+            {"Name": "Arshiya Taj", "City": "Bengaluru", "Class": "N/A"},
+        ],
+        'Kerala': [],
+        'Lakshwadeep': [],
+        'Madhya Pradesh': [],
+        'Maharashtra': [
+            {"Name": "Zabir", "City": "Ratnagiri", "Class": "N/A"},
+            {"Name": "Kazi Mohammad", "City": "Ratnagiri", "Class": "N/A"},
+            {"Name": "Saba Shaikh", "City": "Mumbai", "Class": "N/A"},
+        ],
+        'Manipur': [],
+        'Meghalaya': [],
+        'Mizoram': [],
+        'Nagaland': [],
+        'Orissa': [],
+        'Punjab': [],
+        'Rajasthan': [
+            {"Name": "Iram & Ayan", "City": "Jaipur", "Class": "N/A"},
+        ],
+        'Sikkim': [],
+        'Tamil Nadu': [
+            {"Name": "Liyana & Hasna", "City": "Nagapattinam", "Class": "N/A"},
+            {"Name": "Nusrath", "City": "Chennai", "Class": "N/A"},
+            {"Name": "Rashida & Rasina", "City": "Avaniyapuram", "Class": "N/A"},
+        ],
+        'Telengana': [
+            {"Name": "Zareen", "City": "Hyderabad", "Class": "N/A"},
+            {"Name": "Asma & Nazima", "City": "Hyderabad", "Class": "N/A"},
+            {"Name": "Aamir", "City": "Hyderabad", "Class": "N/A"},
+        ],
+        'Tripura': [],
+        'Uttar Pradesh': [
+            {"Name": "Nida & Huzaifa", "City": "Shikarpur", "Class": "N/A"},
+            {"Name": "Namra & Aisha Noor", "City": "Saharanpur", "Class": "N/A"},
+            {"Name": "Arsalan & Rumaisa", "City": "Rampur", "Class": "N/A"},
+            {"Name": "Subhan & Amaan", "City": "Muzaffarnagar", "Class": "N/A"},
+            {"Name": "Shaan", "City": "Muzaffarnagar", "Class": "N/A"},
+            {"Name": "Alina", "City": "Muzaffarnagar", "Class": "N/A"},
+            {"Name": "Umar & Basit", "City": "Lucknow", "Class": "N/A"},
+            {"Name": "Haider", "City": "Lucknow", "Class": "N/A"},
+            {"Name": "Arshad & Basit", "City": "Lucknow", "Class": "N/A"},
+            {"Name": "Abu Omama & Abu Omar", "City": "Azamgarh", "Class": "N/A"},
+            {"Name": "Alveena, Atif, Ayaz & Aqsa", "City": "Atrauli", "Class": "N/A"},
+
+        ],
+        'Uttaranchal': [],
+        'West Bengal': [
+            {"Name": "Sahil & Shamir", "City": "Howrah", "Class": "N/A"},
+        ]
+    };
+
+    return students;
+}
